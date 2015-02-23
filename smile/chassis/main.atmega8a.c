e2aadb56600bf010530098819d2e1044e86ba38a
Jan Kubalek <janmkubalek@gmail.com>
2015-02-23 14:43:10 +0100

    Base of comminication protocol...

--- smile/chassis/main.atmega8a.c
+++ smile/chassis/main.atmega8a.c
@@ -6,16 +6,37 @@
 #include <util/atomic.h>
 #include <util/delay.h>
 
-#include "lib/TWI_slave.h"
-#include "lib/ENCODER_quadrature.h"
+#include "lib/mega/TWI_slave.h"
+#include "lib/mega/ENCODER_quadrature.h"
 
-union doublebyte {
-	uint16_t value;
-	uint8_t byte[2];
-};
+#define PROTO_HEAD     5
+#define PROTO_DATA     0
+#define PROTO_HEAD_GET 0b11100000
+#define PROTO_DATA_GET 0b00011111
+
+#define PROTO_INSTR_READ         (0b100 << PROTO_HEAD)
+#define PROTO_INSTR_RESET        (0b010 << PROTO_HEAD)
+#define PROTO_INSTR_SET_FORWARD  (0b101 << PROTO_HEAD)
+#define PROTO_INSTR_SET_BACKWARD (0b110 << PROTO_HEAD)
+
+typedef union {
+	volatile int32_t value;
+	volatile int8_t byte[4];
+} quadbyte_t;
 
 
 
+unsigned char message_buff[TWI_BUFFER_SIZE] = {0};
+Encoder_t enc = {0};
+quadbyte_t data;
+
+ISR(INT0_vect, ISR_NOBLOCK) {
+	encoder_inta(&enc);
+}
+
+ISR(INT1_vect, ISR_NOBLOCK) {
+	encoder_intb(&enc);
+}
 
 void init() {
 	// wait for stab..
@@ -32,57 +53,56 @@ void init() {
 }
 
 
-Encoder_t enc = {0};
-
-ISR(INT0_vect, ISR_NOBLOCK) {
-	encoder_inta(&enc);
-}
-
-ISR(INT1_vect, ISR_NOBLOCK) {
-	encoder_intb(&enc);
-}
-
-
-
 int main(void) {
 	TWI_Slave_Initialise(0b00000100);
 	init();
 	_delay_ms(100);
 
-	DDRD = 0b00000011;
-	PORTD = 0b00000011;
+	DDRD  |= (1 << 7);
+	PORTD |= (1 << 7);
 
-
-	unsigned char p[5] = {1,2,3,4,5};
-	unsigned char p2[5] = {2,2,3,4,5};
-	//TWI_Start_Transceiver_With_Data(p2, 1);
-
+	TWI_Start_Transceiver();
 	while(1) {
-
-		if(enc.counter_a > 31000 || enc.counter_a < -31000) {
-			enc.counter_a = 0;
-		}
-		if(enc.counter_b > 31000 || enc.counter_b < -31000) {
-			enc.counter_b = 0;
-		}
-			if(enc.direction == Encoder_DIRECTION_BACKWARD) {
-				PORTD = 0b00000011;
-			}
-			if(enc.direction == Encoder_DIRECTION_FORWARD) {
-				PORTD = 0b00000000;
-			}
-
 		if(!TWI_Transceiver_Busy()) {
-			if(enc.direction == Encoder_DIRECTION_BACKWARD) {
-				TWI_Start_Transceiver_With_Data(p, 1);
-				PORTD = 0b00000011;
-			}
-			if(enc.direction == Encoder_DIRECTION_FORWARD) {
-				TWI_Start_Transceiver_With_Data(p2, 1);
-				PORTD = 0b00000000;
+			if(TWI_statusReg.RxDataInBuf){
+				TWI_Get_Data_From_Transceiver(message_buff, 1);
+				switch(message_buff[0] & PROTO_HEAD_GET) {
+					case PROTO_INSTR_READ:
+						ATOMIC_BLOCK(ATOMIC_FORCEON) {
+							data.value =  enc.counter_b;
+						}
+						message_buff[0] = data.byte[0];
+						message_buff[1] = data.byte[1];
+						message_buff[2] = data.byte[2];
+						message_buff[3] = data.byte[3];
+						TWI_Start_Transceiver_With_Data(message_buff, 4);
+						message_buff[0] = 0;
+						break;
+					case PROTO_INSTR_SET_FORWARD:
+						uint8_t data = message_buff[0] & PROTO_DATA_GET;
+						break;
+					case PROTO_INSTR_SET_BACKWARD:
+						uint8_t data = message_buff[0] & PROTO_DATA_GET;
+						break;
+					case PROTO_INSTR_RESET:
+						ATOMIC_BLOCK(ATOMIC_FORCEON) {
+							enc.counter_a = 0;
+							enc.counter_b = 0;
+							message_buff[0] = 0;
+						}
+						break;
+					default:
+						break;
+				}
 			}
 		}
 		
+		if(enc.direction == Encoder_DIRECTION_BACKWARD) {
+			PORTD |= (1 << 7);
+		}
+		if(enc.direction == Encoder_DIRECTION_FORWARD) {
+			PORTD = 0b00000000;
+		}
 	}
 
 	return 0;
