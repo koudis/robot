09b9d4c0cbf0f13752c6c73f31c4c0fe662ba8af
penickamx@gmail.com <penickamx@gmail.com>
2015-05-05 22:57:04 +0200

    Added directory with documentation to smile/doc

---
+++ smile/doc/locals.mk
@@ -0,0 +1,11 @@
+# -------------------------------------------------
+# LOCALS
+# variables for make and paths to executables
+#
+# add path to executable if program is stored in some nonstandard place or add namo if program is instaled with apt or make install
+# and executable is in search path
+# -------------------------------------------------
+
+INKSCAPE=inkscape
+PDFLATEX=pdflatex
+EAGLE=
